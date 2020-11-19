178f2a9573c2e99edb2b20c11dfaa5b0039a2768
AlyAssem <alyassem1997@gmail.com>
2020-11-19 10:35:55 +0300

    added animation intro

---
+++ frontend/src/components/Intro.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+
+const Intro = () => {
+  const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+
+  useEffect(() => {
+    tl.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
+    tl.to('.slider', { y: '-100%', duration: 1.5, delay: 1 });
+    tl.to('.intro', { y: '-100%', duration: 1 }, '-=1');
+  }, []);
+  return (
+    <>
+      <div className='intro'>
+        <div className='intro-text'>
+          <h1 className='hide'>
+            <span className='text'>Selling fresh grocery</span>
+          </h1>
+          <h1 className='hide'>
+            <span className='text'>For Everyday</span>
+          </h1>
+          <h1 className='hide'>
+            <span className='text'>people.</span>
+          </h1>
+        </div>
+      </div>
+      <div className='slider'></div>
+    </>
+  );
+};
+
+export default Intro;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import './bootstrap.min.css';
-
 import './index.css';
 import App from './App';
+import Intro from './components/Intro';
 
 ReactDOM.render(
   <Provider store={store}>
+    <Intro />
     <App />
   </Provider>,
   document.getElementById('root')
