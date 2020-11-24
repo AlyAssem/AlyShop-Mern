06ec7b8f1d1a55e9322ead96fed99f3fc97248c0
AlyAssem <alyassem1997@gmail.com>
2020-11-24 14:23:46 +0300

    added dependency to the useEffect in intro.js

--- frontend/src/components/Intro.js
+++ frontend/src/components/Intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
     tl.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
     tl.to('.slider', { y: '-100%', duration: 1.5, delay: 1 });
     tl.to('.intro', { y: '-100%', duration: 1 }, '-=1');
-  }, []);
+  }, [tl]);
   return (
     <>
       <div className='intro'>
